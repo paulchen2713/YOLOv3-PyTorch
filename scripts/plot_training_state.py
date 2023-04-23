97aa9c4ab793cef9a2ffc50000903fdf84994441
Paul <95068443+paulchen2713@users.noreply.github.com>
2023-04-23 20:58:30 +0800

    Add files via upload

--- scripts/plot_training_state.py
+++ scripts/plot_training_state.py
@@ -1,7 +1,25 @@
-# 
+# -*- coding: utf-8 -*-
+"""
+Created on Wen Apr 12 16:58:30 2023
+
+@patch: 
+    2023.04.12
+    2023.04.23
+
+@author: Paul
+@file: plot_training_state.py
+@dependencies:
+    env pt3.8
+    python==3.8.16
+    matplotlib==3.6.2
+
+Plot the log messages
+"""
+
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from datetime import date as date_function
+import config
 
 # print(f"{date_function.today()}")
 
@@ -16,10 +34,15 @@ logs = [
     '2023-04-07.txt',
     '2023-04-15.txt',
     '2023-04-16.txt',
+    '2023-04-22.txt',
+    '2023-04-23.txt',
 ]
-log_index = 1
+log_index = 4
 
-# the file tree structure:
+# make sure we are using valid list subscripts
+assert log_index <= len(logs)
+
+# the file tree structure for the training logs:
 """
 D:/Datasets/RADA/RD_JPG/training_logs>tree
 D:.
@@ -36,22 +59,25 @@ D:.
     └─object_accuracy
 """
 
+# the file tree structure for the plots:
+"""
+D:\Datasets\RADA\RD_JPG\stats_figures>tree
+D:.
+├─0316
+├─0327
+├─0407
+├─0415
+├─0416
+├─0422
+└─0423
+"""
 
-# training statistics
-actual_loss = []  # the actual loss for every batch, the total number would be 'epoch x split'
-mean_loss = []    # store the average loss for every epoch 
-train_class_acc = []   # store train class accuracy
-train_no_obj_acc = []  # store train object accuracy
-train_obj_acc = []     # store train object accuracy
+
+def get_file_content(file_name):
+    with open(file_name) as f:
+        return [line.strip() for line in f]
 
 
-# testing statistics
-test_class_acc = []    # store test class accuracy
-test_no_obj_acc = []   # store test object accuracy
-test_obj_acc = []      # store test object accuracy
-mAP = []
-
-# 
 def load_data(file_path):
     with open(file_path, "r", encoding="utf-8") as text_file:
         # print(f"current file: {file_path}")
@@ -61,88 +87,133 @@ def load_data(file_path):
     return data
 
 
-def load_losses(file_path):
-    with open(file_path, "r", encoding="utf-8") as text_file:
-        # print(f"current file: {file_path}")
-        data = []
-        for line in text_file:
-            data.append(line)
-    return data
+# read the training and testing statistics results
+mAP = load_data(PATH + f"mAP/{logs[log_index]}") # store 
+
+# store the actual loss for every batch, the total number would be 'epoch x split', 
+# split = num_train_samples=6000 / batch_size=20 = 300
+losses = load_data(PATH + f"train/losses/{logs[log_index]}") 
+# store the average loss for every epoch
+mean_loss = load_data(PATH + f"train/mean_loss/{logs[log_index]}")
+
+train_class_acc = load_data(PATH + f"train/class_accuracy/{logs[log_index]}")       # store train class accuracy
+train_no_obj_acc = load_data(PATH + f"train/no_object_accuracy/{logs[log_index]}")  # store train no object accuracy
+train_obj_acc = load_data(PATH + f"train/object_accuracy/{logs[log_index]}")        # store train object accuracy
+
+test_class_acc = load_data(PATH + f"test/class_accuracy/{logs[log_index]}")         # store test class accuracy
+test_no_obj_acc = load_data(PATH + f"test/no_object_accuracy/{logs[log_index]}")    # store test no object accuracy
+test_obj_acc = load_data(PATH + f"test/object_accuracy/{logs[log_index]}")          # store test object accuracy
 
 
-def my_plot(x, y, title, y_label, line_color, line_marker):
+def my_plot(x, y, title, x_label, y_label, line_color, line_marker):
     plt.plot(x, y, color=line_color, marker=line_marker)
     
+    # plot the node values
     # for xy in zip(x, y):
     #     plt.annotate('(%d, %.4f)' % xy, xy=xy)
     
-    plt.xlabel('epochs')
+    plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
     plt.show()
 
 
-
-
-
-
-# read and plot the training statistics results
-mAP = load_data(PATH + f"mAP/{logs[log_index]}")
-losses = load_data(PATH + f"train/losses/{logs[log_index]}")
-mean_loss = load_data(PATH + f"train/mean_loss/{logs[log_index]}")
-train_class_acc = load_data(PATH + f"train/class_accuracy/{logs[log_index]}")
-train_no_obj_acc = load_data(PATH + f"train/no_object_accuracy/{logs[log_index]}")
-train_obj_acc = load_data(PATH + f"train/object_accuracy/{logs[log_index]}")
-
-
-# read and plot mAP 
 def plot_mAP():
     # print(f"len(mAP): {len(mAP)}")
     # print(mAP)
-    my_plot([i*100 for i in range(1, len(mAP) + 1)], mAP, 'mean Average Precision', 'Area Under the Curve', 'tab:red', 'x')
+    my_plot(
+        x=[i*100 for i in range(1, len(mAP) + 1)], 
+        y=mAP, 
+        title='mean Average Precision', 
+        x_label='epochs', y_label='Area Under the Curve', 
+        line_color='tab:red', line_marker='x',
+    )
 
 
 def plot_train_results():
-    # my_plot([j for j in range(1, len(losses) + 1)], losses, 'training loss for every batch', 'loss value', 'red', '')
-    my_plot([j for j in range(1, len(mean_loss) + 1)], mean_loss, 'mean training loss', 'loss value', 'red', '')
-    my_plot([j for j in range(1, len(train_class_acc) + 1)], train_class_acc, 'train class accuracy', 'accuracy', 'cornflowerblue', '')
-    my_plot([j for j in range(1, len(train_no_obj_acc) + 1)], train_no_obj_acc, 'train no obj accuracy', 'accuracy', 'royalblue', '')
-    my_plot([j for j in range(1, len(train_obj_acc) + 1)], train_obj_acc, 'train obj accuracy', 'accuracy', 'blue', '')
+    my_plot(
+        x=[j for j in range(1, len(losses) + 1)], 
+        y=losses, 
+        title='training loss for every batch', 
+        x_label='epochs', y_label='loss value', 
+        line_color='red', line_marker='',
+    )
+    my_plot(
+        x=[j for j in range(1, len(mean_loss) + 1)], 
+        y=mean_loss, 
+        title='mean training loss',
+        x_label='epochs', y_label='loss value', 
+        line_color='red', line_marker='',
+    )
+    my_plot(
+        x=[j for j in range(1, len(train_class_acc) + 1)], 
+        y=train_class_acc, 
+        title='train class accuracy',
+        x_label='epochs', y_label='accuracy', 
+        line_color='cornflowerblue', line_marker='',
+    )
+    my_plot(
+        x=[j for j in range(1, len(train_no_obj_acc) + 1)], 
+        y=train_no_obj_acc, 
+        title='train no obj accuracy',
+        x_label='epochs', y_label='accuracy', 
+        line_color='royalblue', line_marker='',
+    )
+    my_plot(
+        x=[j for j in range(1, len(train_obj_acc) + 1)], 
+        y=train_obj_acc, 
+        title='train obj accuracy', 
+        x_label='epochs', y_label='accuracy', 
+        line_color='blue', line_marker='',
+    )
 
-
-# read and plot the testing statistics results
-test_class_acc = load_data(PATH + f"test/class_accuracy/{logs[log_index]}")
-test_no_obj_acc = load_data(PATH + f"test/no_object_accuracy/{logs[log_index]}")
-test_obj_acc = load_data(PATH + f"test/object_accuracy/{logs[log_index]}")
 
 test_point = 10
 def plot_test_results():
-    my_plot([j*test_point for j in range(1, len(test_class_acc) + 1)], test_class_acc, 'test class accuracy', 'accuracy', 'darkturquoise', '')
-    my_plot([j*test_point for j in range(1, len(test_no_obj_acc) + 1)], test_no_obj_acc, 'test no obj accuracy', 'accuracy', 'deepskyblue', '')
-    my_plot([j*test_point for j in range(1, len(test_obj_acc) + 1)], test_obj_acc, 'test obj accuracy', 'accuracy', 'dodgerblue', '')
+    my_plot(
+        x=[j*test_point for j in range(1, len(test_class_acc) + 1)], 
+        y=test_class_acc, 
+        title='test class accuracy', 
+        x_label='epochs', y_label='accuracy', 
+        line_color='darkturquoise', line_marker='',
+    )
+    my_plot(
+        x=[j*test_point for j in range(1, len(test_no_obj_acc) + 1)], 
+        y=test_no_obj_acc, 
+        title='test no obj accuracy', 
+        x_label='epochs', y_label='accuracy', 
+        line_color='deepskyblue', line_marker='',
+    )
+    my_plot(
+        x=[j*test_point for j in range(1, len(test_obj_acc) + 1)], 
+        y=test_obj_acc, 
+        title='test obj accuracy', 
+        x_label='epochs', y_label='accuracy', 
+        line_color='dodgerblue', line_marker='',
+    )
+
+def print_stats():
+    print(f"max mAP: {max(mAP)}")
+    print(f"mean mAP: {sum(mAP) / len(mAP)}")
+
+    print(f"max training loss: {max(losses)}")
+    print(f"min training loss: {min(losses)}")
+
+    print(f"max training loss on average: {max(mean_loss)}")
+    print(f"min training loss on average: {min(mean_loss)}")
+
+    print(f"min training accuracy: {min(train_obj_acc)}")
+    print(f"max training accuracy: {max(train_obj_acc)}")
+
+    print(f"min testing accuracy: {min(test_obj_acc)}")
+    print(f"max testing accuracy: {max(test_obj_acc)}")
 
 
-print(f"max mAP: {max(mAP)}")
-print(f"mean mAP: {sum(mAP) / len(mAP)}")
-
-print(f"max training loss: {max(losses)}")
-print(f"min training loss: {min(losses)}")
-
-print(f"max training loss on average: {max(mean_loss)}")
-print(f"min training loss on average: {min(mean_loss)}")
-
-print(f"min training accuracy: {min(train_obj_acc)}")
-print(f"max training accuracy: {max(train_obj_acc)}")
-
-print(f"min testing accuracy: {min(test_obj_acc)}")
-print(f"max testing accuracy: {max(test_obj_acc)}")
-
-
-my_plot([j for j in range(1, len(losses) + 1)], losses, 'training loss for every batch', 'loss value', 'red', '')
-
-# plot_mAP()
-# plot_train_results()
-# plot_test_results()
+if __name__ == "__main__":
+    print_stats()
+    plot_mAP()
+    plot_train_results()
+    plot_test_results()
 
 
 
