cfbad8dea76bae911da3c024396ef835b5f047a3
Paul <95068443+paulchen2713@users.noreply.github.com>
2023-06-27 23:18:38 +0800

    Add files via upload

--- YOLOv3-CARRADA/train.py
+++ YOLOv3-CARRADA/train.py
@@ -72,7 +72,7 @@ def seed_everything(seed=33):
 # Using a unified 'log_file_name' for all file objects is necessary because if the training process runs across several days, 
 # the log messages for the same training will be split into several files with different dates as their file names. However, 
 # they actually belong in the same file. All log files will be named as the start date of the training.
-log_file_name = '2023-06-27-2' # TODO date_function.today() 
+log_file_name = '2023-06-27-3' # TODO date_function.today() 
 
 # we are checking whether '<log_file_name>.txt' file exists in the 'losses' folder
 file2check = config.DATASET + f'training_logs/train/losses/{log_file_name}.txt'  
@@ -358,10 +358,14 @@ if __name__ == "__main__":
     main()
 
     # test()
+
+    # NOTE 7-fold
+    # 2023-06-27-3  epoch: 100   duration:   hours  WEIGHT_DECAY = 1e-4  LEARNING_RATE = 15e-5  max mAP:  0.  ##split 0 + smaller model-9
+
     
     # NOTE 5-fold 
     # 2023-06-27-2  epoch: 100   duration:  6.1737 hours  WEIGHT_DECAY = 1e-4  LEARNING_RATE = 15e-5  max mAP:  0.4250  ##split 4 + smaller model-9
-    # 2023-06-27-1  epoch: 100   duration:   hours  WEIGHT_DECAY = 1e-4  LEARNING_RATE = 15e-5  max mAP:  0.  ##split 3 + smaller model-8
+    # 2023-06-27-1  epoch: 100   duration:  4.0348 hours  WEIGHT_DECAY = 1e-4  LEARNING_RATE = 15e-5  max mAP:  0.4437  ##split 3 + smaller model-8
     # 2023-06-26-2  epoch: 100   duration:  4.0543 hours  WEIGHT_DECAY = 1e-4  LEARNING_RATE = 15e-5  max mAP:  0.3944  ##split 2 + smaller model-7
     # 2023-06-26-1  epoch: 100   duration: 13.1282 hours  WEIGHT_DECAY = 1e-4  LEARNING_RATE = 15e-5  max mAP:  0.3873  ##split 1 + smaller model-6
     # 2023-06-19-2  epoch: 100   duration: 10.8377 hours  WEIGHT_DECAY = 1e-4  LEARNING_RATE = 15e-5  max mAP:  0.3631  ##split 0 + smaller model-5
