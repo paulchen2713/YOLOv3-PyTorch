76d5b3c049b415146c09004d5f173a94a189f5d4
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2023-07-11 11:26:28 +0800

    Add files via upload

--- scripts/rename.py
+++ scripts/rename.py
@@ -1,22 +1,26 @@
 
 import torchvision.transforms as T # for resizing the images
 from PIL import Image              # for loading and saving the images
-import os
-from os import listdir
+import os                          # for grabbing the files
 
 isResized = False
-date = '2022.06.22'
-seq_path = 'D:/BeginnerPythonProjects/renaming_files/input/'
+date = '2023.03.08'
+PATH = f"C:/Users/paulc/Downloads/rename_files/"
+
 
 def rename_files():
-    count = 1
-    for images in os.listdir(seq_path):
-        # check if the image ends with png
-        if (images.endswith(f".jpg") or images.endswith(f".png")):
-            # print(count, seq_path + images)
+    count_jpg, count_png = 1, 1
+
+    src_path = PATH + f"source/"
+    dst_path = PATH + f"output/"
+
+    for images in os.listdir(src_path):
+        # check if the image ends with '.jpg' or '.jpeg'
+        if images.endswith(f".jpg") or images.endswith(f".jpeg"):
+            # print(count_jpg, src_path + images)
 
             # read the input image
-            img = Image.open(seq_path + images)
+            img = Image.open(src_path + images)
 
             if isResized == True:
                 print("Resizing and renaming images!")
@@ -35,10 +39,24 @@ def rename_files():
                 # print(f"resized image size: {after}")
 
             # overwrite the original image with the resized one
-            img = img.save(f'D:/BeginnerPythonProjects/renaming_files/output/{date}-0{count}.png')
+            img = img.save(dst_path + f'{date}-{count_jpg}.jpg')
             
-            print(count)
-            count += 1
+            print(count_jpg)
+            count_jpg += 1
+    
+    for images in os.listdir(src_path):
+        # check if the image ends with '.png'
+        if images.endswith(f".png"):
+            # print(count_png, src_path + images)
+
+            # read the input image
+            img = Image.open(src_path + images)
+
+            # overwrite the original image with the resized one
+            img = img.save(dst_path + f'{date}-{count_jpg}.png')
+
+            print(count_png)
+            count_png += 1
 
 
 
