08ddde74b84f86cfdc103d9a535b79d865a9db6c
Shao-Heng <95068443+paulchen2713@users.noreply.github.com>
2023-07-13 08:17:05 +0800

    Add files via upload

--- scripts/plot_training_state.py
+++ scripts/plot_training_state.py
@@ -123,6 +123,7 @@ logs = [
     '2023-06-28-5',  # 6 + small model-10
     '2023-07-11-1',  # NOTE 8-fold: 0 + small model-11
     '2023-07-12-1',  # 1 + small model-11
+    '2023-07-12-2',  # 2 + small model-12
 ]
 
 log_index = len(logs) - 1
